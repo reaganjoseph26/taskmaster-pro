01189f034e27c782661152030ee5b8cb1c59e424
Racer <reaganjoseph26@yahoo.com>
2020-11-12 10:57:35 -0500

    automated task auditing

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -276,8 +276,11 @@ var auditTask = function (taskEl) {
   }
 };
 
-
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+  auditTask(el);
+})
+}, (1000 * 60) * 30);
 
 
 // remove all tasks
